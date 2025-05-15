2d7b7888b8b3e954984d8ad67c659d8163d5590a
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-05-15 16:46:54 +0530

    MPU9250_asukiaaa.h lib add

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,14 @@
-#include <Arduino.h>
-int r1 = 7;
-int r2 = 8;
-int r3 = 9;
-int r4 = 10;
-
+#include<Arduino.h>
+int sensorPin = A0; // AO pin connected to A0
 
 void setup() {
-  pinMode(r1, OUTPUT);
-  digitalWrite(r1, HIGH);
-  pinMode(r2, OUTPUT);
-  digitalWrite(r2, HIGH);
-  pinMode(r3, OUTPUT);
-  digitalWrite(r3, HIGH);
-  pinMode(r4, OUTPUT);
-  digitalWrite(r4, HIGH);
+  Serial.begin(9600);
 }
 
 void loop() {
-  digitalWrite(r1, LOW);
-  delay(2000);
-  digitalWrite(r2, LOW);
-  delay(2000);
-  digitalWrite(r3, LOW);
-  delay(2000);
-  digitalWrite(r4, LOW);
-  delay(4000);
-  digitalWrite(r1, HIGH);
-  delay(2000);
-  digitalWrite(r2, HIGH);
-  delay(2000);
-  digitalWrite(r3, HIGH);
-  delay(2000);
-  digitalWrite(r4, HIGH);
+  int sensorValue = analogRead(sensorPin); // Read analog value
+  Serial.print("Sensor Value: ");
+  Serial.println(sensorValue);
 
-
-
-
+  delay(500); // Small delay for readability
 }
