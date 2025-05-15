a167b9bf581af8296a960a22757ad4b9f934d33d
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-05-15 17:34:36 +0530

    gyro

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,39 @@
-#include<Arduino.h>
-int sensorPin = A0; // AO pin connected to A0
+#include <Wire.h>
+#include <MPU9250_asukiaaa.h>
+
+MPU9250_asukiaaa mySensor;
 
 void setup() {
   Serial.begin(9600);
+  Wire.begin();
+
+  mySensor.setWire(&Wire);
+  mySensor.beginAccel();
+  mySensor.beginGyro();
+  mySensor.beginMag();
+
+  Serial.println("MPU9250 Initialized");
 }
 
 void loop() {
-  int sensorValue = analogRead(sensorPin); // Read analog value
-  Serial.print("Sensor Value: ");
-  Serial.println(sensorValue);
+  mySensor.accelUpdate();
+  mySensor.gyroUpdate();
+  mySensor.magUpdate();
 
-  delay(500); // Small delay for readability
+  Serial.print("Accel: ");
+  Serial.print(mySensor.accelX()); Serial.print(", ");
+  Serial.print(mySensor.accelY()); Serial.print(", ");
+  Serial.print(mySensor.accelZ());
+
+  Serial.print(" | Gyro: ");
+  Serial.print(mySensor.gyroX()); Serial.print(", ");
+  Serial.print(mySensor.gyroY()); Serial.print(", ");
+  Serial.print(mySensor.gyroZ());
+
+  Serial.print(" | Mag: ");
+  Serial.print(mySensor.magX()); Serial.print(", ");
+  Serial.print(mySensor.magY()); Serial.print(", ");
+  Serial.println(mySensor.magZ());
+
+  delay(500);
 }
