c1c9f38db57fab4f9efe390a5a0d241bf97cb50d
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-05-15 16:30:11 +0530

    RELAY TEST

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,32 @@ int r4 = 10;
 void setup() {
   pinMode(r1, OUTPUT);
   digitalWrite(r1, HIGH);
+  pinMode(r2, OUTPUT);
+  digitalWrite(r2, HIGH);
+  pinMode(r3, OUTPUT);
+  digitalWrite(r3, HIGH);
+  pinMode(r4, OUTPUT);
+  digitalWrite(r4, HIGH);
 }
 
 void loop() {
-  digitalWrite(7, HIGH);
-  delay(2000);;
-  digitalWrite(7, LOW);
+  digitalWrite(r1, LOW);
   delay(2000);
+  digitalWrite(r2, LOW);
+  delay(2000);
+  digitalWrite(r3, LOW);
+  delay(2000);
+  digitalWrite(r4, LOW);
+  delay(4000);
+  digitalWrite(r1, HIGH);
+  delay(2000);
+  digitalWrite(r2, HIGH);
+  delay(2000);
+  digitalWrite(r3, HIGH);
+  delay(2000);
+  digitalWrite(r4, HIGH);
+
+
+
 
 }
