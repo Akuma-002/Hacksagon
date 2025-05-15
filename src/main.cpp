cf0b1a3da5520df3ad9b1dfb08e4612a8db52183
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-05-15 16:19:52 +0530

    r

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,19 @@
 #include <Arduino.h>
-
+int r1 = 7;
+int r2 = 8;
+int r3 = 9;
+int r4 = 10;
 
 
 void setup() {
-  pinMode(13, OUTPUT);
+  pinMode(r1, OUTPUT);
+  digitalWrite(r1, HIGH);
 }
 
 void loop() {
-  digitalWrite(13, HIGH);
-  delay(500);;
-  digitalWrite(13, LOW);
-  delay(500);
+  digitalWrite(7, HIGH);
+  delay(2000);;
+  digitalWrite(7, LOW);
+  delay(2000);
+
 }
