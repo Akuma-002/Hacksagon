2b312ccf507a362159bcbbdc0b66895599dc034a
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-05-18 12:23:57 +0530

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,35 @@
+#include<Arduino.h>
 #include <Wire.h>
-#include <MPU9250_asukiaaa.h>
+#include <MPU9250.h>
 
-MPU9250_asukiaaa mySensor;
+#define LED_PIN 13
+
+MPU9250 mpu;
 
 void setup() {
   Serial.begin(9600);
   Wire.begin();
+  pinMode(LED_PIN, OUTPUT);
 
-  mySensor.setWire(&Wire);
-  mySensor.beginAccel();
-  mySensor.beginGyro();
-  mySensor.beginMag();
-
-  Serial.println("MPU9250 Initialized");
+  // Initialize MPU9250
+  if (!mpu.setup(0x68)) {  // change to 0x69 if your AD0 pin is high
+    Serial.println("MPU9250 connection failed!");
+    while (1);
+  }
 }
 
 void loop() {
-  mySensor.accelUpdate();
-  mySensor.gyroUpdate();
-  mySensor.magUpdate();
+  if (mpu.update()) {
+    float ax = mpu.getAccX(); // acceleration in g (gravity)
+    Serial.print("AccX: ");
+    Serial.println(ax);
 
-  Serial.print("Accel: ");
-  Serial.print(mySensor.accelX()); Serial.print(", ");
-  Serial.print(mySensor.accelY()); Serial.print(", ");
-  Serial.print(mySensor.accelZ());
-
-  Serial.print(" | Gyro: ");
-  Serial.print(mySensor.gyroX()); Serial.print(", ");
-  Serial.print(mySensor.gyroY()); Serial.print(", ");
-  Serial.print(mySensor.gyroZ());
-
-  Serial.print(" | Mag: ");
-  Serial.print(mySensor.magX()); Serial.print(", ");
-  Serial.print(mySensor.magY()); Serial.print(", ");
-  Serial.println(mySensor.magZ());
-
-  delay(500);
+    // Adjust threshold as needed; positive X means right tilt if sensor is flat
+    if (ax > 0.3) { // 0.3g threshold, tune as needed
+      digitalWrite(LED_PIN, HIGH);
+    } else {
+      digitalWrite(LED_PIN, LOW);
+    }
+  }
+  delay(50);
 }
